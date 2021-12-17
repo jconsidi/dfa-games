e3b073cf3e00195a6dc63ee07848c7636a4bfc36
Jeffrey Considine <jconsidi@gmail.com>
2021-12-16 22:29:38 -0500

    + perft: allow FEN to be passed in command line

--- src/perft.cpp
+++ src/perft.cpp
@@ -5,28 +5,34 @@
 
 #include "chess.h"
 
+const char *FEN_DEFAULT = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 const uint64_t PERFT_EXPECTED[] = {1, 20, 400, 8902, 197281, 4865609};
 
 uint64_t perft(const Board& board, int depth);
 
-int main()
+int main(int argc, char **argv)
 {
-  Board board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+  const char *fen = (argc >= 2) ? argv[1] : FEN_DEFAULT;
+
+  Board board(fen);
   std::cout << board;
 
-  for(int depth = 0; depth < 5; ++depth)
+  for(int depth = 0; depth < 6; ++depth)
     {
       uint64_t output = perft(board, depth);
       std::cout << "depth " << depth << ": " << output << std::endl;
 
-      if(depth < sizeof(PERFT_EXPECTED) / sizeof(uint64_t))
+      uint64_t output_expected = 0ULL;
+      if((fen == FEN_DEFAULT) && (depth < sizeof(PERFT_EXPECTED) / sizeof(uint64_t)))
 	{
-	  uint64_t output_expected = PERFT_EXPECTED[depth];
-	  if(output != output_expected)
-	    {
-	      std::cerr << "depth " << depth << ": " << output_expected << " expected" << std::endl;
-	      return 1;
-	    }
+	  output_expected = PERFT_EXPECTED[depth];
+	}
+      // LATER: allow expected output on command line
+
+      if((output_expected) && (output != output_expected))
+	{
+	  std::cerr << "depth " << depth << ": " << output_expected << " expected" << std::endl;
+	  return 1;
 	}
     }
 
