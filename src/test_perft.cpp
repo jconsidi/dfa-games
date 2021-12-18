68a6fbd642194d3699f9b2152ae57c8c42dc2dbe
Jeffrey Considine <jconsidi@gmail.com>
2021-12-18 17:13:10 -0500

    + test_perft: deepen a few tests

--- src/test_perft.cpp
+++ src/test_perft.cpp
@@ -49,11 +49,11 @@ int main()
       test("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", std::vector<uint64_t>({48, 2039, 97862, 4085603}));
 
       // Position 3
-      test("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", std::vector<uint64_t>({14, 191, 2812, 43238, 674624}));
+      test("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", std::vector<uint64_t>({14, 191, 2812, 43238, 674624, 11030083}));
 
       // Position 4
-      test("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", std::vector<uint64_t>({6, 264, 9467, 422333}));
-      test("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", std::vector<uint64_t>({6, 264, 9467, 422333}));
+      test("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", std::vector<uint64_t>({6, 264, 9467, 422333, 15833292}));
+      test("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", std::vector<uint64_t>({6, 264, 9467, 422333, 15833292}));
 
       // Position 5
       test("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", std::vector<uint64_t>({44, 1486, 62379, 2103487}));
