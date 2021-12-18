b844d2d142598a89837ae166e304dff26d5c1835
Jeffrey Considine <jconsidi@gmail.com>
2021-12-17 23:15:37 -0500

    + test_generate_moves -> generate_moves

--- src/generate_moves.cpp
+++ src/generate_moves.cpp
@@ -1,19 +1,22 @@
-// test_generate_moves.cpp
+// generate_moves.cpp
 
 #include <cstdint>
 #include <iostream>
 
 #include "chess.h"
 
-int main()
+int main(int argc, char **argv)
 {
-  Board board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+  const char *fen = (argc >= 2) ? argv[1] : INITIAL_FEN;
+  Board board(fen);
+  std::cout << board << std::endl;
 
   Board moves[CHESS_MAX_MOVES];
   int num_moves = board.generate_moves(moves);
 
   for(int i = 0; i < num_moves; ++i)
     {
+      std::cout << "MOVE " << i << std::endl;
       std::cout << moves[i] << std::endl;
     }
 
