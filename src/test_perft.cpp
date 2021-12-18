a5a14c96dc3c6e91d058a5be7493d6b3fa6d0659
Jeffrey Considine <jconsidi@gmail.com>
2021-12-18 17:09:02 -0500

    + test_perft: more test cases from https://www.chessprogramming.org/Perft_Results

--- src/test_perft.cpp
+++ src/test_perft.cpp
@@ -42,6 +42,24 @@ int main()
 
       // initial position
       test(INITIAL_FEN, std::vector<uint64_t>({20, 400, 8902, 197281, 4865609}));
+
+      // positions from https://www.chessprogramming.org/Perft_Results
+
+      // Position 2 aka Kiwipete
+      test("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", std::vector<uint64_t>({48, 2039, 97862, 4085603}));
+
+      // Position 3
+      test("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", std::vector<uint64_t>({14, 191, 2812, 43238, 674624}));
+
+      // Position 4
+      test("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", std::vector<uint64_t>({6, 264, 9467, 422333}));
+      test("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", std::vector<uint64_t>({6, 264, 9467, 422333}));
+
+      // Position 5
+      test("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", std::vector<uint64_t>({44, 1486, 62379, 2103487}));
+
+      // Position 6
+      test("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", std::vector<uint64_t>({46, 2079, 89890, 3894594}));
     }
   catch(std::logic_error e)
     {
