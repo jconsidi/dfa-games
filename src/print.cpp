1afcdb1c046fc97931042d3812c112d90e65baa6
Jeffrey Considine <jconsidi@gmail.com>
2021-12-17 23:04:41 -0500

    + print program

---
+++ src/print.cpp
@@ -0,0 +1,14 @@
+// print.cpp
+
+#include <iostream>
+
+#include "chess.h"
+
+int main(int argc, char **argv)
+{
+  const char *fen = (argc > 1) ? argv[1] : INITIAL_FEN;
+  Board board(fen);
+  std::cout << board;
+  
+  return 0;
+}
