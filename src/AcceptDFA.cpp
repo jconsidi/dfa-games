3932f6e77f5708d4d3b4282632293e9a16c0786a
Jeffrey Considine <jconsidi@gmail.com>
2021-12-29 00:04:14 -0500

    + AcceptDFA implemented

--- src/AcceptDFA.cpp
+++ src/AcceptDFA.cpp
@@ -4,5 +4,24 @@
 
 AcceptDFA::AcceptDFA()
 {
-  throw std::logic_error("AcceptDFA::AcceptDFA not implemented");
+  uint64_t next_states[DFA_MAX];
+
+  // penultimate state
+
+  for(int i = 0; i < DFA_MAX; ++i)
+    {
+      next_states[i] = (1 << DFA_MAX) - 1;
+    }
+  add_state(62, next_states);
+
+  // internal states
+
+  for(int i = 0; i < DFA_MAX; ++i)
+    {
+      next_states[i] = 0;
+    }
+  for(int layer = 61; layer >= 0; --layer)
+    {
+      add_state(layer, next_states);
+    }
 }
