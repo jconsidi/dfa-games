cd539c263d8c46f9e5e464bb36ea7d4c29ff8692
Jeffrey Considine <jconsidi@gmail.com>
2022-01-25 17:17:30 -0500

    + test_chess_game: drop perft_u test

--- src/test_chess_game.cpp
+++ src/test_chess_game.cpp
@@ -77,28 +77,5 @@ int main()
   assert(initial_positions_check);
   assert(initial_positions_check->size() == 1);
 
-  std::cout << " perft_u" << std::endl;
-
-  std::vector<uint64_t> expected_sizes({20, 400});
-
-  shared_dfa_ptr current_positions = initial_positions;
-  for(int previous_ply = 0; previous_ply < expected_sizes.size(); ++previous_ply)
-    {
-      int side_to_move = previous_ply % 2;
-
-      current_positions = chess.get_moves_forward(side_to_move, current_positions);
-      int current_size = current_positions->size();
-      int current_states = current_positions->states();
-
-      std::cout << "  ply " << (previous_ply + 1) << ": " << current_size << " positions, " << current_states << " states" << std::endl;
-
-      int expected_size = expected_sizes[previous_ply];
-      if(current_size != expected_size)
-	{
-	  std::cerr << "  ply " << (previous_ply + 1) << ": " << expected_size << " expected" << std::endl;
-	  return 1;
-	}
-    }
-
   return 0;
 }
