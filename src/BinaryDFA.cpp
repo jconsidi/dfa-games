0d5ee122e16b70bc076efb3c54ea3d7d1ff65ec8
Jeffrey Considine <jconsidi@gmail.com>
2022-02-05 14:49:24 -0500

    + BinaryDFA: use priority queue for merging more than two DFAs to avoid quadratic overhead

--- src/BinaryDFA.cpp
+++ src/BinaryDFA.cpp
@@ -5,6 +5,7 @@
 #include <assert.h>
 
 #include <iostream>
+#include <queue>
 
 template <int ndim, int... shape_pack>
 BinaryDFA<ndim, shape_pack...>::BinaryDFA(const DFA<ndim, shape_pack...>& left_in,
@@ -62,13 +63,7 @@ BinaryDFA<ndim, shape_pack...>::BinaryDFA(const std::vector<std::shared_ptr<cons
   // counting states until two DFAs left, then merge the last two for
   // this DFA.
 
-  std::vector<std::shared_ptr<const DFA<ndim, shape_pack...>>> dfas_temp;
-  for(int i = 0; i < dfas_in.size(); ++i)
-    {
-      dfas_temp.push_back(dfas_in[i]);
-    }
-
-  struct
+  typedef struct reverse_less
   {
     bool operator()(std::shared_ptr<const DFA<ndim, shape_pack...>> a,
 		    std::shared_ptr<const DFA<ndim, shape_pack...>> b) const
@@ -77,37 +72,43 @@ BinaryDFA<ndim, shape_pack...>::BinaryDFA(const std::vector<std::shared_ptr<cons
     }
   } reverse_less;
 
-  while(dfas_temp.size() > 2)
+  std::priority_queue<
+    std::shared_ptr<const DFA<ndim, shape_pack...>>,
+    std::vector<std::shared_ptr<const DFA<ndim, shape_pack...>>>,
+    reverse_less> dfa_queue;
+
+  for(int i = 0; i < dfas_in.size(); ++i)
     {
-      int temp_size = dfas_temp.size();
+      dfa_queue.push(dfas_in[i]);
+    }
 
-      // partial sort so last two DFAs have the least positions
-      std::nth_element(dfas_temp.begin(),
-		       dfas_temp.begin() + (temp_size - 2),
-		       dfas_temp.end(),
-		       reverse_less);
-
-      std::shared_ptr<const DFA<ndim, shape_pack...>>& second_last = dfas_temp[temp_size - 2];
-      std::shared_ptr<const DFA<ndim, shape_pack...>> last = dfas_temp[temp_size - 1];
+  while(dfa_queue.size() > 2)
+    {
+      std::shared_ptr<const DFA<ndim, shape_pack...>> last = dfa_queue.top();
+      dfa_queue.pop();
+      std::shared_ptr<const DFA<ndim, shape_pack...>> second_last = dfa_queue.top();
+      dfa_queue.pop();
 
       if(second_last->states() >= 1024)
 	{
 	  std::cerr << "  merging DFAs with " << last->states() << " states and " << second_last->states() << " states" << std::endl;
 	}
-      second_last = std::shared_ptr<const DFA<ndim, shape_pack...>>(new BinaryDFA(*second_last, *last, leaf_func));
-      dfas_temp.pop_back();
-
-      assert(dfas_temp.size() == temp_size - 1);
+      dfa_queue.push(std::shared_ptr<const DFA<ndim, shape_pack...>>(new BinaryDFA(*second_last, *last, leaf_func)));
     }
+  assert(dfa_queue.size() == 2);
 
-  assert(dfas_temp.size() == 2);
+  std::shared_ptr<const DFA<ndim, shape_pack...>> d0 = dfa_queue.top();
+  dfa_queue.pop();
+  std::shared_ptr<const DFA<ndim, shape_pack...>> d1 = dfa_queue.top();
+  dfa_queue.pop();
+  assert(dfa_queue.size() == 0);
 
-  if((dfas_temp[0]->states() >= 1024) || (dfas_temp[1]->states() >= 1024))
+  if((d0->states() >= 1024) || (d1->states() >= 1024))
     {
-      std::cerr << "  merging DFAs with " << dfas_temp[0]->states() << " states and " << dfas_temp[1]->states() << " states" << std::endl;
+      std::cerr << "  merging DFAs with " << d0->states() << " states and " << d1->states() << " states" << std::endl;
     }
 
-  BinaryBuildCache cache(*(dfas_temp[0]), *(dfas_temp[1]), leaf_func);
+  BinaryBuildCache cache(*d0, *d1, leaf_func);
   binary_build(0, 0, 0, cache);
   assert(this->ready());
 
