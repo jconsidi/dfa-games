8573e2e53c6c7b169f444c569bb6636fd749b898
Jeffrey Considine <jconsidi@gmail.com>
2022-08-07 11:31:28 -0400

    + test_chess_game : added profiling

--- src/test_chess_game.cpp
+++ src/test_chess_game.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "ChessGame.h"
+#include "Profile.h"
 #include "test_utils.h"
 
 typedef typename ChessGame::shared_dfa_ptr shared_dfa_ptr;
@@ -67,40 +68,53 @@ shared_dfa_ptr initial_positions_manual()
 
 int main()
 {
+  Profile profile("test_chess_game");
   ChessGame chess;
 
   // chess-specific tests
 
+  profile.tic("get_initial_positions");
   shared_dfa_ptr initial_positions = chess.get_initial_positions();
   assert(initial_positions);
   assert(initial_positions->size() == 1);
 
+  profile.tic("initial_positions_manual");
   shared_dfa_ptr initial_positions_expected = initial_positions_manual();
   assert(initial_positions_expected);
   assert(initial_positions_expected->size() == 1);
 
+  profile.tic("initial_positions_check");
   shared_dfa_ptr initial_positions_check(new ChessGame::intersection_dfa_type(*initial_positions, *initial_positions_expected));
   assert(initial_positions_check);
   assert(initial_positions_check->size() == 1);
 
+  profile.tic("black_check_board");
   Board black_check_board("rnbq1bnr/ppppkppp/8/4p3/8/BP6/P1PPPPPP/RN1QKBNR w KQkq - 0 1");
   assert(black_check_board.is_check(SIDE_BLACK));
   shared_dfa_ptr black_check_dfa = ChessGame::from_board(black_check_board);
 
+  profile.tic("black_threat_12");
   shared_dfa_ptr black_threat_12 = chess.get_threat_positions(SIDE_BLACK, 12);
+
+  profile.tic("black_threat_confirmed");
   shared_dfa_ptr black_threat_confirmed(new ChessGame::intersection_dfa_type(*black_check_dfa, *black_threat_12));
   assert(black_threat_confirmed->size() > 0);
 
+  profile.tic("black_check_positions");
   shared_dfa_ptr black_check_positions = chess.get_check_positions(SIDE_BLACK);
+
+  profile.tic("black_check_confirmed");
   shared_dfa_ptr black_check_confirmed(new ChessGame::intersection_dfa_type(*black_check_dfa, *black_check_positions));
   assert(black_check_confirmed->size() > 0);
 
   // generic tests
 
+  profile.tic("positions_expected");
   std::vector<size_t> positions_expected({1, 20, 400});
   test_forward(chess, positions_expected);
 
   // done
 
+  profile.tic("done");
   return 0;
 }
