29c78c4a3d60ce7f1c95217e73f4fbe7ae220df2
Jeffrey Considine <jconsidi@gmail.com>
2022-08-28 20:13:45 -0400

    + test_perft_u : manual mode only expands one ply

--- src/test_perft_u.cpp
+++ src/test_perft_u.cpp
@@ -155,7 +155,7 @@ int main(int argc, char **argv)
 	  for(int i = 1; i < argc; ++i)
 	    {
 	      std::string fen = argv[i];
-	      test("manual", fen, 2);
+	      test("manual", fen, 1);
 	    }
 
 	  return 0;
