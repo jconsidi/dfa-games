52a01c1f3fa1520760fff9ff22a9c4c70240f24a
Jeffrey Considine <jconsidi@gmail.com>
2022-10-08 21:40:00 -0400

    + DFAUtil : added short-circuit logic to get_union()

--- src/DFAUtil.cpp
+++ src/DFAUtil.cpp
@@ -75,6 +75,34 @@ typename DFAUtil<ndim, shape_pack...>::shared_dfa_ptr DFAUtil<ndim, shape_pack..
 template <int ndim, int... shape_pack>
 typename DFAUtil<ndim, shape_pack...>::shared_dfa_ptr DFAUtil<ndim, shape_pack...>::get_union(DFAUtil<ndim, shape_pack...>::shared_dfa_ptr left_in, DFAUtil<ndim, shape_pack...>::shared_dfa_ptr right_in)
 {
+  std::optional<bool> left_constant = check_constant(left_in);
+  if(left_constant.has_value())
+    {
+      bool left_value = *left_constant;
+      if(left_value)
+	{
+	  return get_accept();
+	}
+      else
+	{
+	  return right_in;
+	}
+    }
+
+  std::optional<bool> right_constant = check_constant(right_in);
+  if(right_constant.has_value())
+    {
+      bool right_value = *right_constant;
+      if(right_value)
+	{
+	  return get_accept();
+	}
+      else
+	{
+	  return left_in;
+	}
+    }
+
   return shared_dfa_ptr(new UnionDFA<ndim, shape_pack...>(*left_in, *right_in));
 }
 
