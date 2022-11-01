ec6260c6e0db8a6b922603c0c24c136f229879a5
Jeffrey Considine <jconsidi@gmail.com>
2022-10-31 23:00:54 -0400

    + test_normal_nim_game : exhaustively check Nim positions against analytical solution

--- src/test_normal_nim_game.cpp
+++ src/test_normal_nim_game.cpp
@@ -1,7 +1,9 @@
 // test_normal_nim_game.cpp
 
 #include <iostream>
+#include <vector>
 
+#include "DFAUtil.h"
 #include "NormalNimGame.h"
 #include "test_utils.h"
 
@@ -12,9 +14,66 @@ int test()
 
   NormalNimGame<n, shape_pack...> nim;
 
+  int shape[] = {shape_pack...};
+
+  // calculate maximum number of moves
+
+  int max_moves = 0;
+  for(int layer = 0; layer < n; ++layer)
+    {
+      max_moves += shape[layer];
+    }
+
   // run generic tests
 
-  test_backward(nim, n * 16, n % 2);
+  test_backward(nim, max_moves, n % 2);
+
+  // run analytical tests
+
+  auto positions_losing = nim.get_positions_losing(0, max_moves);
+  auto positions_winning = nim.get_positions_winning(0, max_moves);
+
+  auto positions_shared = DFAUtil<n, shape_pack...>::get_intersection(positions_losing, positions_winning);
+  assert(positions_shared->is_constant(false));
+
+  auto positions_combined = DFAUtil<n, shape_pack...>::get_union(positions_losing, positions_winning);
+  assert(positions_combined->is_constant(true));
+
+  size_t shape_product = 1;
+  for(int layer = 0; layer < n; ++layer)
+    {
+      shape_product *= shape[layer];
+    }
+
+  for(size_t i = 0; i < shape_product; ++i)
+    {
+      size_t temp = i;
+      std::vector<int> position_characters;
+      for(int layer = 0; layer < n; ++layer)
+	{
+	  position_characters.push_back(temp % shape[layer]);
+	  temp /= shape[layer];
+	}
+      DFAString<n, shape_pack...> position = DFAString<n, shape_pack...>(position_characters);
+
+      int characters_xored = 0;
+      for(int layer = 0; layer < n; ++layer)
+	{
+	  characters_xored ^= position_characters[layer];
+	}
+
+      bool losing_expected = characters_xored == 0;
+      if(losing_expected)
+	{
+	  assert(positions_losing->contains(position));
+	}
+      else
+	{
+	  assert(positions_winning->contains(position));
+	}
+    }
+
+  // done
 
   return 0;
 }
