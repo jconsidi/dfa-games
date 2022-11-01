d7c282fbd983942d901be0900b754423929e6d37
Jeffrey Considine <jconsidi@gmail.com>
2022-10-31 22:23:01 -0400

    + test_normal_nim_game : tweaked testing to take in DFA parameters directly

--- src/test_normal_nim_game.cpp
+++ src/test_normal_nim_game.cpp
@@ -5,12 +5,12 @@
 #include "NormalNimGame.h"
 #include "test_utils.h"
 
-template<int n, class T>
+template<int n, int... shape_pack>
 int test()
 {
   std::cout << "TESTING " << n << std::endl;
 
-  T nim;
+  NormalNimGame<n, shape_pack...> nim;
 
   // run generic tests
 
@@ -21,10 +21,10 @@ int test()
 
 int main()
 {
-  test<1, NormalNim1Game>();
-  test<2, NormalNim2Game>();
-  test<3, NormalNim3Game>();
-  test<4, NormalNim4Game>();
+  test<NORMALNIM1_DFA_PARAMS>();
+  test<NORMALNIM2_DFA_PARAMS>();
+  test<NORMALNIM3_DFA_PARAMS>();
+  test<NORMALNIM4_DFA_PARAMS>();
 
   return 0;
 }
