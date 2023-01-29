1dc796a82594e55f5b11da595dc97cabe6f27746
Jeffrey Considine <jconsidi@gmail.com>
2023-01-29 16:42:53 -0500

    + print : add functionality to print game saved dfas

--- src/print.cpp
+++ src/print.cpp
@@ -2,13 +2,38 @@
 
 #include <iostream>
 
+#include "DFA.h"
+#include "Game.h"
 #include "chess.h"
+#include "test_utils.h"
 
 int main(int argc, char **argv)
 {
-  const char *fen = (argc > 1) ? argv[1] : INITIAL_FEN;
-  Board board(fen);
-  std::cout << board;
+  if(argc == 2)
+    {
+      // assume chess FEN string
+      const char *fen = (argc > 1) ? argv[1] : INITIAL_FEN;
+      Board board(fen);
+      std::cout << board;
+    }
+  else if(argc == 3)
+    {
+      // args = game, DFA name
+      std::string game_name(argv[1]);
+      Game *game = get_game(game_name);
+
+      std::string positions_name(argv[2]);
+      shared_dfa_ptr positions = game->load(positions_name);
+
+      for(auto iter = positions->cbegin();
+	  iter < positions->cend();
+	  ++iter)
+	{
+	  DFAString position_string = *iter;
+	  std::cout << game->position_to_string(position_string) << std::endl;
+	  std::cout << std::endl;
+	}
+    }
 
   return 0;
 }
