ec254a3034e047cd2334597cd5d94974ac6ece7a
Jeffrey Considine <jconsidi@gmail.com>
2023-11-30 14:33:12 -0500

    + Flashsort : refactored and added flashsort_partition()

--- src/Flashsort.cpp
+++ src/Flashsort.cpp
@@ -2,26 +2,18 @@
 
 #include "Flashsort.h"
 
+#include <cassert>
+
 #include "Profile.h"
 
 // flashsort in-situ permutation
 // https://en.m.wikipedia.org/wiki/Flashsort
 
 template<class T, class K>
-void flashsort_permutation(T *begin, T *end, std::function<K(const T&)> key_func)
+std::vector<size_t> _flashsort_count_cumulative(T *begin, T *end, std::function<K(const T&)> key_func)
 {
-  Profile profile("flashsort_permutation");
+  std::vector<K> auxiliary = {0};
 
-  if(begin + 1 >= end)
-    {
-      return;
-    }
-
-  // count occurrences of each key
-
-  profile.tic("auxiliary count");
-
-  std::vector<K> auxiliary;
   for(T *p = begin; p < end; ++p)
     {
       K key = key_func(*p);
@@ -34,21 +26,24 @@ void flashsort_permutation(T *begin, T *end, std::function<K(const T&)> key_func
 
       auxiliary[key] += 1;
     }
-  assert(auxiliary.size() >= 1);
+
+  for(size_t k = 1; k < auxiliary.size(); ++k)
+    {
+      auxiliary[k] += auxiliary[k - 1];
+    }
+
+  return auxiliary;
+}
+
+template<class T, class K>
+void _flashsort_permute(T *begin, T *end, std::function<K(const T&)> key_func, std::vector<size_t>& auxiliary)
+{
+  Profile profile("_flashsort_permute");
 
   if(auxiliary.size() == 1)
     {
       // only key value was zero
       return;
-    }
-
-  // convert from counts to cumulative counts
-
-  profile.tic("auxiliary cumulative");
-
-  for(K k = 1; k < auxiliary.size(); ++k)
-    {
-      auxiliary[k] += auxiliary[k-1];
     }
 
   assert(auxiliary[auxiliary.size() - 1] == end - begin);
@@ -131,4 +126,60 @@ void flashsort_permutation(T *begin, T *end, std::function<K(const T&)> key_func
 #endif
 }
 
+template<class T, class K>
+std::vector<T *> flashsort_partition(T *begin, T *end, std::function<K(const T&)> key_func)
+{
+  // returns vector partitioning input range into different key
+  // values. uses more working space than flashsort_permutation.
+
+  Profile profile("flashsort_partition");
+
+  assert(begin <= end);
+
+  // count occurrences of each key
+
+  std::vector<size_t> auxiliary = _flashsort_count_cumulative<T, K>(begin, end, key_func);
+
+  // output prep
+
+  std::vector<T *> output;
+  output.reserve(auxiliary.size() + 1);
+  output.push_back(begin);
+  for(size_t i = 0; i < auxiliary.size(); ++i)
+    {
+      output.push_back(begin + auxiliary[i]);
+    }
+  assert(output.size() == auxiliary.size() + 1);
+  assert(output.back() == end);
+
+  // permute elements into correct partitions
+
+  _flashsort_permute<T, K>(begin, end, key_func, auxiliary);
+
+  return output;
+}
+
+template<class T, class K>
+void flashsort_permutation(T *begin, T *end, std::function<K(const T&)> key_func)
+{
+  Profile profile("flashsort_permutation");
+
+  if(begin + 1 >= end)
+    {
+      return;
+    }
+
+  // cumulative key counts
+
+  profile.tic("auxiliary count");
+
+  std::vector<size_t> auxiliary = _flashsort_count_cumulative<T, K>(begin, end, key_func);
+
+  // permute elements into correct partitions
+
+  _flashsort_permute(begin, end, key_func, auxiliary);
+}
+
 // template instantiations
+
+template std::vector<size_t *> flashsort_partition(size_t *, size_t *, std::function<size_t(const size_t&)>);
--- src/Flashsort.h
+++ src/Flashsort.h
@@ -6,6 +6,9 @@
 #include <functional>
 
 template<class T, class K>
+std::vector<T *> flashsort_partition(T *begin, T *end, std::function<K(const T&)> key_func);
+
+template<class T, class K>
   void flashsort_permutation(T *begin, T *end, std::function<K(const T&)> key_func);
 
 #endif
