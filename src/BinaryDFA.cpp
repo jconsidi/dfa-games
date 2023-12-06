04d0ccad4bdd06c2a0a141f06dec50a29b2cb9c2
Jeffrey Considine <jconsidi@gmail.com>
2023-12-06 11:34:25 -0500

    + BinaryDFA : more concise unique profiling

--- src/BinaryDFA.cpp
+++ src/BinaryDFA.cpp
@@ -532,8 +532,8 @@ void BinaryDFA::build_quadratic_mmap(const DFA& left_in,
 				next_left_size * next_right_size);
       while(unique_queue.size())
 	{
-	  Profile profile2("unique round");
-	  profile2.tic("unique round init");
+	  Profile profile2("unique");
+	  profile2.tic("init");
 
 	  auto unique_todo = unique_queue.back();
 	  unique_queue.pop_back();
@@ -567,7 +567,7 @@ void BinaryDFA::build_quadratic_mmap(const DFA& left_in,
 	      // sort approach and make a bitmap of which ones are
 	      // present.
 
-	      profile2.tic("unique round count");
+	      profile2.tic("count");
 
 	      std::vector<bool> values_present(value_width, false);
 	      for(size_t *v_iter = begin; v_iter < end; ++v_iter)
@@ -592,7 +592,7 @@ void BinaryDFA::build_quadratic_mmap(const DFA& left_in,
 	    {
 	      // "small" range, so just handle directly
 
-	      profile2.tic("unique round base");
+	      profile2.tic("base");
 
 	      end = TRY_PARALLEL_3(std::remove_if, begin, end, remove_func);
 	      sync_if_big(range_bytes);
@@ -611,7 +611,7 @@ void BinaryDFA::build_quadratic_mmap(const DFA& left_in,
 
 	  // use flashsort permutation to partition by value
 
-	  profile2.tic("unique round partition");
+	  profile2.tic("partition");
 
 	  size_t target_buckets = 1024;
 	  size_t divisor = (value_width + target_buckets - 1) / target_buckets;
