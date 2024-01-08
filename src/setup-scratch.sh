42f5ef13570e51e6435e8f49339e4c8e2cb7c2a0
Jeffrey Considine <jconsidi@gmail.com>
2024-01-08 10:39:51 -0500

    + setup-scratch script : setup required scratch subdirectories

---
+++ src/setup-scratch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+cd scratch
+
+mkdir -p binarydfa
+mkdir -p change_cache
+mkdir -p dfas_by_hash
+mkdir -p intersection_cache
+mkdir -p move_nodes
+mkdir -p temp
+mkdir -p union_cache
