2eeed8d310d76556bdd37ef10b33b6f2f18499e8
Jeffrey Considine <jconsidi@gmail.com>
2024-01-10 14:33:08 -0500

    + validate_losing and validate_winning   (to support validating backward solves)

--- src/validate_utils.cpp
+++ src/validate_utils.cpp
@@ -30,6 +30,60 @@ bool validate_equal(shared_dfa_ptr dfa_a, shared_dfa_ptr dfa_b)
 	  DFAUtil::get_difference(dfa_b, dfa_a)->is_constant(false));
 }
 
+bool validate_losing(const Game& game, int side_to_move, shared_dfa_ptr curr_losing, shared_dfa_ptr next_winning, shared_dfa_ptr base_losing, int max_examples)
+{
+  std::cout << " CHECKING EXAMPLES" << std::endl;
+
+  int losing_examples = 0;
+  for(auto iter = curr_losing->cbegin();
+	  (iter < curr_losing->cend()) && (losing_examples < max_examples);
+	  ++iter, ++losing_examples)
+    {
+      DFAString position(*iter);
+
+      int result = game.validate_result(side_to_move, position);
+      if(result > 0)
+	{
+	  std::cerr << "LOSING EXAMPLE ACTUALLY WON" << std::endl;
+	  std::cerr << game.position_to_string(position) << std::endl;
+	  return false;
+	}
+
+      if(base_losing->contains(position))
+	{
+	  continue;
+	}
+      else if(result < 0)
+	{
+	  std::cerr << "LOST EXAMPLE MISSED BY BASE CASE" << std::endl;
+	  std::cerr << game.position_to_string(position) << std::endl;
+	  return false;
+	}
+
+      std::vector<DFAString> moves = game.validate_moves(side_to_move, position);
+      if(moves.size() == 0)
+	{
+	  std::cerr << "NOT LOST EXAMPLE WITHOUT MOVES" << std::endl;
+	  std::cerr << game.position_to_string(position) << std::endl;
+	  return false;
+	}
+
+      for(const DFAString& move : moves)
+	{
+	  if(!next_winning->contains(move))
+	    {
+	      std::cerr << "LOSING EXAMPLE WITHOUT FORCED LOSS" << std::endl;
+	      std::cerr << game.position_to_string(position) << std::endl;
+	      return false;
+	    }
+	}
+    }
+
+  std::cout << "checked " << losing_examples << " losing examples." << std::endl;
+
+  return true;
+}
+
 bool validate_partition(shared_dfa_ptr target, std::vector<shared_dfa_ptr> partition)
 {
   shared_dfa_ptr union_check = DFAUtil::get_union_vector(target->get_shape(), partition);
@@ -116,4 +170,65 @@ bool validate_subset(shared_dfa_ptr dfa_a, shared_dfa_ptr dfa_b)
 {
   shared_dfa_ptr subset_check = DFAUtil::get_difference(dfa_a, dfa_b);
   return subset_check->is_constant(0);
+}
+
+bool validate_winning(const Game& game, int side_to_move, shared_dfa_ptr curr_winning, shared_dfa_ptr next_losing, shared_dfa_ptr base_winning, int max_examples)
+{
+  std::cout << " CHECK EXAMPLES" << std::endl;
+
+  int winning_examples = 0;
+  for(auto iter = curr_winning->cbegin();
+      (iter < curr_winning->cend()) && (winning_examples < max_examples);
+      ++iter, ++winning_examples)
+    {
+      DFAString position(*iter);
+
+      int result = game.validate_result(side_to_move, position);
+      if(result < 0)
+	{
+	  std::cerr << "WINNING EXAMPLE ACTUALLY LOST" << std::endl;
+	  std::cerr << game.position_to_string(position) << std::endl;
+	  return false;
+	}
+
+      if(base_winning->contains(position))
+	{
+	  continue;
+	}
+      else if(result > 0)
+	{
+	  std::cerr << "WON EXAMPLE MISSED BY BASE CASE" << std::endl;
+	  std::cerr << game.position_to_string(position) << std::endl;
+	  return false;
+	}
+
+      std::vector<DFAString> moves = game.validate_moves(side_to_move, position);
+      if(moves.size() == 0)
+	{
+	  std::cerr << "NOT WON EXAMPLE WITHOUT MOVES" << std::endl;
+	  std::cerr << game.position_to_string(position) << std::endl;
+	  return false;
+	}
+
+      bool found_win = false;
+      for(const DFAString& move : moves)
+	{
+	  if(next_losing->contains(move))
+	    {
+	      found_win = true;
+	      break;
+	    }
+	}
+
+      if(!found_win)
+	{
+	  std::cerr << "WINNING EXAMPLE WITHOUT WINNING MOVE" << std::endl;
+	  std::cerr << game.position_to_string(position) << std::endl;
+	  return false;
+	}
+    }
+
+  std::cout << "checked " << winning_examples << " winning examples." << std::endl;
+
+  return true;
 }
--- src/validate_utils.h
+++ src/validate_utils.h
@@ -10,8 +10,10 @@
 
 bool validate_disjoint(shared_dfa_ptr dfa_a, shared_dfa_ptr dfa_b);
 bool validate_equal(shared_dfa_ptr, shared_dfa_ptr);
+bool validate_losing(const Game&, int, shared_dfa_ptr, shared_dfa_ptr, shared_dfa_ptr, int);
 bool validate_partition(shared_dfa_ptr, std::vector<shared_dfa_ptr>);
 bool validate_result(const Game&, int, shared_dfa_ptr, int, int);
 bool validate_subset(shared_dfa_ptr, shared_dfa_ptr);
+bool validate_winning(const Game&, int, shared_dfa_ptr, shared_dfa_ptr, shared_dfa_ptr, int);
 
 #endif
