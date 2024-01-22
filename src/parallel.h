c6e15284ba99c30615d6c5ec5d5c63a0ea790ce7
Jeffrey Considine <jconsidi@gmail.com>
2024-01-22 10:44:25 -0500

    + TRY_PARALLEL_6

--- src/parallel.h
+++ src/parallel.h
@@ -8,11 +8,13 @@
 #define TRY_PARALLEL_3(f, a, b, c) f(std::execution::par_unseq, a, b, c)
 #define TRY_PARALLEL_4(f, a, b, c, d) f(std::execution::par_unseq, a, b, c, d)
 #define TRY_PARALLEL_5(f, a, b, c, d, e) f(std::execution::par_unseq, a, b, c, d, e)
+#define TRY_PARALLEL_6(f, a, b, c, d, e, g) f(std::execution::par_unseq, a, b, c, d, e, g)
 #else
 #define TRY_PARALLEL_2(f, a, b) f(a, b)
 #define TRY_PARALLEL_3(f, a, b, c) f(a, b, c)
 #define TRY_PARALLEL_4(f, a, b, c, d) f(a, b, c, d)
 #define TRY_PARALLEL_5(f, a, b, c, d, e) f(a, b, c, d, e)
+#define TRY_PARALLEL_6(f, a, b, c, d, e, g) f(a, b, c, d, e, g)
 #endif
 
 #endif
