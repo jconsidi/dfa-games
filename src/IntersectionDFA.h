3083d373cde03f4eef097c5c4d47222bda64bda4
Jeffrey Considine <jconsidi@gmail.com>
2024-08-30 22:54:28 -0400

    + IntersectionDFA : dropped vestigial intersection_mask

--- src/IntersectionDFA.h
+++ src/IntersectionDFA.h
@@ -7,8 +7,6 @@
 
 class IntersectionDFA : public BinaryDFA
 {
-  static dfa_state_t intersection_mask(dfa_state_t left_mask, dfa_state_t right_mask);
-
  public:
 
   IntersectionDFA(const DFA&, const DFA&);
