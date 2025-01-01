e5af2505cdda94f9160782e4af6ea41a4f381d87
Jeffrey Considine <jconsidi@gmail.com>
2025-01-01 00:39:51 -0500

    + InverseDFA : switch to build_layer and fix bug with layer sizes >= 2^31

--- src/InverseDFA.cpp
+++ src/InverseDFA.cpp
@@ -18,36 +18,27 @@ InverseDFA::InverseDFA(const DFA& dfa_in)
 
   // same transitions as original DFA, but final masks are inverted.
 
-  int last_layer_size = dfa_in.get_layer_size(ndim - 1);
-  for(int old_state_index = 2; old_state_index < last_layer_size; ++old_state_index)
+  for(int layer = ndim - 1; layer >= 0; --layer)
     {
-      DFATransitionsReference old_transitions = dfa_in.get_transitions(ndim - 1, old_state_index);
-      dfa_state_t new_state_index = this->add_state_by_function(ndim - 1, [&](int i){return !old_transitions[i];});
-      assert(new_state_index == old_state_index);
-    }
+      int layer_shape = dfa_in.get_layer_shape(layer);
 
-  for(int layer = ndim - 2; layer >= 0; --layer)
-    {
-      int layer_size = dfa_in.get_layer_size(layer);
-
-      for(int old_state_index = 2; old_state_index < layer_size; ++old_state_index)
-	{
-	  DFATransitionsReference old_transitions = dfa_in.get_transitions(layer, old_state_index);
-	  dfa_state_t new_state_index = this->add_state_by_function(layer, [&](int i)
-	  {
+      build_layer(layer, dfa_in.get_layer_size(layer), [&](dfa_state_t old_state_index, dfa_state_t *transitions_out)
+      {
+        DFATransitionsReference old_transitions = dfa_in.get_transitions(layer, old_state_index);
+        for(int i = 0; i < layer_shape; ++i)
+          {
 	    if(old_transitions[i] < 2)
 	      {
 		// accept/reject case
-		return 1 - old_transitions[i];
+		transitions_out[i] = 1 - old_transitions[i];
 	      }
 	    else
 	      {
 		// everything else has inverse in place
-		return old_transitions[i];
+		transitions_out[i] = old_transitions[i];
 	      }
-	  });
-	  assert(new_state_index == old_state_index);
-	}
+          }
+      });
     }
 
   this->set_initial_state(dfa_in.get_initial_state());
