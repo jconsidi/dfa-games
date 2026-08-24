f82e62c5c522519295f682fd75c6777b74138a4f
Jeffrey Considine <jconsidi@gmail.com>
2026-08-24 14:33:43 -0400

    + build_forward_backward_proof : first pass just working in memory

---
+++ src/build_forward_backward_proof.cpp
@@ -0,0 +1,117 @@
+// build_forward_backward_proof.cpp
+
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "DFAUtil.h"
+#include "parallel.h"
+#include "test_utils.h"
+
+std::string get_name(int forward_ply_max, int backward_ply_max, int ply, std::string result)
+{
+  std::ostringstream output;
+  output << "forward_backward";
+  output << ",forward_ply_max=" << std::setfill('0') << std::setw(3) << forward_ply_max;
+  output << ",backward_ply_max=" << std::setfill('0') << std::setw(3) << backward_ply_max;
+  output << ",ply=" << std::setfill('0') << std::setw(3) << ply;
+  output << "," << result;
+
+  return output.str();
+}
+
+shared_dfa_ptr get_positions(const Game& game, int forward_ply_max, int backward_ply_max, int ply, std::string result)
+{
+  std::string name = get_name(forward_ply_max, backward_ply_max, ply, result);
+  std::cout << "LOADING " << name << std::endl;
+
+  shared_dfa_ptr dfa = game.load(name);
+  if(!dfa)
+    {
+      throw std::logic_error("failed loading " + name);
+    }
+  return dfa;
+}
+
+int main(int argc, char **argv)
+{
+  if(argc < 2)
+    {
+      std::cerr << "usage: build_forward_backward_proof GAME_NAME [FORWARD_PLY] [BACKWARD_PLY]\n";
+      return 1;
+    }
+
+  std::string game_name(argv[1]);
+  Game *game = get_game(game_name);
+
+  int forward_ply_max = (argc >= 3) ? atoi(argv[2]) : 100;
+  int backward_ply_max = (argc >= 4) ? atoi(argv[3]) : 0;
+
+  auto initial_positions = game->get_positions_initial();
+  assert(initial_positions->size() == 1);
+
+  DFAString initial_position = *(initial_positions->cbegin());
+
+  shared_dfa_ptr winning_0 = get_positions(*game, forward_ply_max, backward_ply_max, 0, "winning");
+  bool initial_winning = winning_0->contains(initial_position);
+
+  std::vector<DFAString> curr_positions = {initial_position};
+  for(int ply = 0; ply <= forward_ply_max; ++ply)
+    {
+      int side_to_move = ply % 2;
+      bool ply_winning = initial_winning ^ (ply % 2);
+      
+      std::cout << "PLY " << ply << " " << (ply_winning ? "WINNING" : "LOSING") << " " << curr_positions.size() << " positions to prove." << std::endl;
+
+      // load next losing positions if proving wins now.
+      shared_dfa_ptr next_losing = ply_winning ? get_positions(*game, forward_ply_max, backward_ply_max, ply + 1, "losing") : 0;
+      
+      std::vector<DFAString> next_positions;
+      for(const DFAString& curr_position : curr_positions)
+        {
+          std::vector<DFAString> temp_positions = game->validate_moves(side_to_move, curr_position);
+
+          if(ply_winning)
+            {
+              // must have at least one move to a losing position
+              assert(temp_positions.size() > 0);
+              bool found_loss = false;
+              for(const DFAString &temp_position : temp_positions)
+                {
+                  if(next_losing->contains(temp_position))
+                    {
+                      next_positions.push_back(temp_position);
+                      found_loss = true;
+                      break;
+                    }
+                }
+              assert(found_loss);
+            }
+          else // ply is losing
+            {
+              // all moves must go to winning positions
+              for(const DFAString &temp_position : temp_positions)
+                {
+                  next_positions.push_back(temp_position);
+                }
+            }
+        }
+
+      std::cout << "  expanded to " << next_positions.size() << std::endl;
+      TRY_PARALLEL_2(std::sort, next_positions.begin(), next_positions.end());
+      std::cout << "  sorted" << std::endl;
+      auto unique_end = TRY_PARALLEL_2(std::unique, next_positions.begin(), next_positions.end());
+      auto unique_count = unique_end - next_positions.begin();
+      std::cout << "  uniqued to " << unique_count << std::endl;
+      next_positions.resize(unique_count);
+      std::cout << "  resized" << std::endl;
+
+      // prepare for next ply
+      std::swap(curr_positions, next_positions);
+    }
+
+  return 0;
+}
