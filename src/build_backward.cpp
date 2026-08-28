caa674fbe1eae438c1665834bf8c8f1ea0c5250f
Jeffrey Considine <jconsidi@gmail.com>
2026-08-28 13:54:10 -0400

    + build_backward : save incremental winning/losing positions by exact ply

--- src/build_backward.cpp
+++ src/build_backward.cpp
@@ -1,9 +1,11 @@
 // build_backward.cpp
 
+#include <format>
 #include <iostream>
 #include <map>
 #include <string>
 
+#include "DFAUtil.h"
 #include "test_utils.h"
 
 int main(int argc, char **argv)
@@ -32,6 +34,30 @@ int main(int argc, char **argv)
 	  shared_dfa_ptr losing = game->get_positions_losing(side_to_move, ply);
 	  shared_dfa_ptr winning = game->get_positions_winning(side_to_move, ply);
 	  shared_dfa_ptr unknown = game->get_positions_unknown(side_to_move, ply);
+
+          // exact ply counts
+
+          game->load_or_build(std::format("backward,ply={:03d},side={:d},losing", ply, side_to_move), [&]()
+          {
+            if(ply == 0)
+              {
+                return game->get_positions_lost(side_to_move);
+              }
+
+            return DFAUtil::get_difference(losing, game->get_positions_losing(side_to_move, ply - 1));
+          });
+
+          game->load_or_build(std::format("backward,ply={:03d},side={:d},winning", ply, side_to_move), [&]()
+          {
+            if(ply == 0)
+              {
+                return game->get_positions_won(side_to_move);
+              }
+
+            return DFAUtil::get_difference(winning, game->get_positions_winning(side_to_move, ply - 1));
+          });
+
+          // termination check
 
           if(!unknown->is_constant(0))
             {
