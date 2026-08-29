d61da84edc9ab3bb1fa6cd9ffbefcdf1fd4844d1
Jeffrey Considine <jconsidi@gmail.com>
2026-08-29 14:47:44 -0400

    + test_validate : check consistency of validate_moves and validate_result for every configured position

---
+++ src/test_validate.cpp
@@ -0,0 +1,57 @@
+// test_validate.cpp
+
+#include <format>
+#include <iostream>
+
+#include <nlohmann/json.hpp>
+
+#include "DFAUtil.h"
+#include "test_utils.h"
+
+void test_validate_case(const Game& game, const DFAString& position, const nlohmann::json& test_case)
+{
+  int side_to_move = test_case.at("side_to_move").get<int>();
+  std::cout << "SIDE TO MOVE: " << side_to_move << std::endl;
+
+  const auto validate_moves = game.validate_moves(side_to_move, position);
+  const auto validate_result = game.validate_result(side_to_move, position);
+
+  std::cout << "validate_moves() returned " << validate_moves.size() << " moves." << std::endl;
+  if(validate_result)
+    {
+      std::cout << "validate_result() returned " << *validate_result << std::endl;
+    }
+  else
+    {
+      std::cout << "validate_result() returned none." << std::endl;
+    }
+  
+  if(validate_moves.size() == 0)
+    {
+      if(!validate_result)
+        {
+          throw std::logic_error("validate_moves() returned zero moves, but validate_result() returned none.");
+        }
+    }
+  else
+    {
+      if(validate_result)
+        {
+          throw std::logic_error(std::format("validate_moves() returned {:d} moves, but validate_result returned {:d}.", validate_moves.size(), *validate_result));
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+  if(argc > 2)
+    {
+      std::cerr << "usage: test_validate [GAME_NAME]\n";
+      return 1;
+    }
+
+  run_test_positions((argc >= 2) ? std::string(argv[1]) : std::string(""),
+                     test_validate_case);
+
+  return 0;
+}
