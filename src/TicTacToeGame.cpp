d49e4ae726e8cfc8847e162c375f7f5ff6582575
Jeffrey Considine <jconsidi@gmail.com>
2026-08-29 14:46:26 -0400

    + TicTacToeGame : implemented validation

--- src/TicTacToeGame.cpp
+++ src/TicTacToeGame.cpp
@@ -53,3 +53,106 @@ std::string TicTacToeGame::position_to_string(const DFAString& position_in) cons
 
   return builder.str();
 }
+
+std::vector<DFAString> TicTacToeGame::validate_moves(int side_to_move, const DFAString& position) const
+{
+  std::vector<DFAString> output;
+  if(validate_result(side_to_move, position))
+    {
+      return output;
+    }
+
+  int friendly_char = 1 + side_to_move;
+
+  int n2 = n * n;
+  for(int move = 0; move < n2; ++move)
+    {
+      if(position[move] != 0)
+        {
+          continue;
+        }
+
+      std::vector<int> position_new;
+      for(int layer = 0; layer < n2; ++layer)
+        {
+          if(layer == move)
+            {
+              position_new.push_back(friendly_char);
+            }
+          else
+            {
+              position_new.push_back(position[layer]);
+            }
+        }
+
+      output.emplace_back(get_shape(), position_new);
+    }
+
+  return output;
+}
+
+std::optional<int> TicTacToeGame::validate_result(int side_to_move, const DFAString& position) const
+{
+  int hostile_char = 1 + (1 - side_to_move);
+
+  // check for other side having won
+  auto check_helper = [&](int layer_first, int layer_delta)
+  {
+    if(position[layer_first] != hostile_char)
+      {
+        return std::optional<int>();
+      }
+
+    for(int i = 1; i < n; ++i)
+      {
+        if(position[layer_first + i * layer_delta] != hostile_char)
+          {
+            return std::optional<int>();
+          }
+      }
+
+    return std::optional<int>(-1);
+  };
+
+  for(int row = 0; row < n; ++row)
+    {
+      auto row_result = check_helper(calculate_layer(row, 0), 1);
+      if(row_result)
+        {
+          return row_result;
+        }
+    }
+
+  for(int col = 0; col < n; ++col)
+    {
+      auto col_result = check_helper(calculate_layer(0, col), n);
+      if(col_result)
+        {
+          return col_result;
+        }
+    }
+
+  auto diag0_result = check_helper(0, n + 1);
+  if(diag0_result)
+    {
+      return diag0_result;
+    }
+
+  auto diag1_result = check_helper(n - 1, n - 1);
+  if(diag1_result)
+    {
+      return diag1_result;
+    }
+
+  int n2 = n * n;
+  for(int layer = 0; layer < n2; ++layer)
+    {
+      if(position[layer] == 0)
+        {
+          // found empty square
+          return std::optional<int>();
+        }
+    }
+
+  return std::optional<int>(0);
+}
--- src/TicTacToeGame.h
+++ src/TicTacToeGame.h
@@ -13,11 +13,18 @@ class TicTacToeGame
 private:
   int n;
 
+  int calculate_layer(int r, int c) const {return r * n + c;}
+  
  public:
 
   TicTacToeGame(int);
 
   virtual std::string position_to_string(const DFAString&) const;
+
+  // validation
+
+  virtual std::vector<DFAString> validate_moves(int, const DFAString&) const;
+  virtual std::optional<int> validate_result(int, const DFAString&) const;
 };
 
 #endif
