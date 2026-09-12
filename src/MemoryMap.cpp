f2bfbba6f9bfa74a185736057147ef3089aa216d
Jeffrey Considine <jconsidi@gmail.com>
2026-09-12 10:00:03 -0400

    + MemoryMap : more visibility into open failures

--- src/MemoryMap.cpp
+++ src/MemoryMap.cpp
@@ -398,11 +398,8 @@ int MemoryMap<T>::open(int flags, int mode) const
   int fildes = ::open(_filename.c_str(), flags, mode);
   if(fildes == -1)
     {
-      if(errno != ENOENT)
-	{
-	  std::cerr << "open " << _filename << " failed" << std::endl;
-	  perror("open");
-	}
+      std::cerr << "open " << _filename << " failed" << std::endl;
+      perror("open");
       throw std::runtime_error("open() failed");
     }
 
