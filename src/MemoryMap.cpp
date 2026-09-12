b95b9c46f142b1c3b8e14fc7b4f3d579e6b8c5ad
Jeffrey Considine <jconsidi@gmail.com>
2026-09-12 15:41:33 -0400

    Revert "+ MemoryMap : more visibility into open failures"
    This reverts commit f2bfbba6f9bfa74a185736057147ef3089aa216d.

--- src/MemoryMap.cpp
+++ src/MemoryMap.cpp
@@ -398,8 +398,11 @@ int MemoryMap<T>::open(int flags, int mode) const
   int fildes = ::open(_filename.c_str(), flags, mode);
   if(fildes == -1)
     {
-      std::cerr << "open " << _filename << " failed" << std::endl;
-      perror("open");
+      if(errno != ENOENT)
+	{
+	  std::cerr << "open " << _filename << " failed" << std::endl;
+	  perror("open");
+	}
       throw std::runtime_error("open() failed");
     }
 
